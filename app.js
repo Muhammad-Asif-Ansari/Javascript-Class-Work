e91bb29043eeee980f0a366364065921fbbbe12b
Muhammad-Asif-Ansari <muhammadasifansari101@gmail.com>
2024-10-11 11:49:08 +0500

    JavaScript Assignment
    JavaScript Assignment

---
+++ app.js
@@ -0,0 +1,92 @@
+// var date = new Date()
+// var currentDay = date.getDay();
+// console.log(currentDay);
+
+// var arr = ["Sun","Mon","Tue","Wed","Thus","Fri","Sat"]
+// console.log(arr[5]);
+
+
+// var date = new Date()
+// var currentMonth = date.getMonth();
+// console.log(currentMonth);
+
+// var month = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
+// console.log(month[9]);
+
+
+// var date = new Date()
+// var currentDate = date.getDate();
+// console.log(currentDate);
+
+// var date = new Date()
+// var currentYear = date.getFullYear();
+// console.log(currentYear);
+
+
+// var date = new Date()
+// var currentHours = date.getHours();
+// console.log(currentHours);
+
+
+
+// var date = new Date()
+// var currentMinutes = date.getMinutes();
+// console.log(currentMinutes);
+
+// var date = new Date()
+// var currentSeconds = date.getSeconds();
+// console.log(currentSeconds);
+
+
+
+// var date = new Date()
+// var currentMillSeconds = date.getMilliseconds();
+// console.log(currentMillSeconds);
+
+
+// var date = new Date()
+// var currentTime = date.getTime();
+// console.log(currentTime);
+
+
+// var date = new Date()
+// var fullYear = date.setFullYear(2020);
+// console.log(fullYear);
+
+
+// var date = new Date()
+// var month = date.setMonth(2);
+// console.log(month);
+
+// var date = new Date()
+// var date2 = date.setDate(5);
+// console.log(date2);
+
+
+// var date = new Date()
+// var hours = date.setHours(5);
+// console.log(hours);
+
+// var date = new Date()
+// var seconds = date.setSeconds(5);
+// console.log(seconds);
+
+// var date = new Date()
+// var milliSecond = date.setMilliseconds(5);
+// console.log(milliSecond);
+
+// var date = new Date()
+// var minutes = date.setMinutes(5);
+// console.log(minutes);
+
+
+
+// var year1 = new Date("26 August 2002");
+// var year2 = new Date();
+// console.log("My Current age is" , year2.getFullYear() - year1.getFullYear());
+
+
+
+// var year = new Date("26 August 2002");
+// console.log("My Date of Birth is"+ " " + year);
+
